03175b9885a71983353754d67b73144d923c12fd
kokojj <kovjscl976@naver.com>
2024-12-01 20:56:15 +0900

    체크박스

---
+++ form/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain.item;
+
+public enum ItemType {
+    //ENUM 타입
+    BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+}
